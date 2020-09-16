f97b8567dc0bc193514deb395ec8b3926c1cd613
Your Name <akas3524@yahoo.com>
2020-09-16 20:17:09 +0000

    countLetter

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected){ 
+    console.log( `✅✅✅ Assertion Passed: ${actual} === ${expected} `)
+  }
+  else 
+    console.log( ` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `)
+};
+
+const countLetters = function(input){ 
+  let obj={}  
+  let newString = input.split(' ').join('')
+  for(x = 0; x < newString.length; x++) {
+      let  index = newString.charAt(x)
+      if(isNaN(obj[index])){ 
+        obj[index] = 1
+      }
+      else 
+      obj[index] += 1
+  }
+  return obj
+}  
+
+
+
